32c60e9068ebb8cdf98c29276ab49cc34ed7004f
Ehab_Ahmed143 <ehabahmedabdrabo@gmail.com>
2025-10-01 14:40:27 +0300

    craete fetch notes method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,7 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
 
 import 'package:notes_app/cubits/notes_cubit/notes_state.dart';
+import 'package:notes_app/models/note_model.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchNotes() {
+    try {
+      var notesbox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesbox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFaluire(e.toString()));
+    }
+  }
 }
--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
